13b06f0ed9cfba3df19ca451f7b692fbae556149
Ab-Daniel <31863068+Ab-Daniel@users.noreply.github.com>
2018-11-20 10:31:35 -0700

    Create SavingsAccount.java

---
+++ SavingsAccount.java
@@ -0,0 +1,27 @@
+
+public class SavingsAccount extends BankAccount
+{
+	
+	public SavingsAccount(double min,double aRate)
+	{
+		
+		minBalance = min;
+		
+		rate = aRate;
+		
+	}
+	
+	public void addIntrest()
+	{
+		
+		
+		
+	}
+	
+	
+	
+	private double minBalance = 2000;
+	
+	private double rate = 4;
+	
+}
