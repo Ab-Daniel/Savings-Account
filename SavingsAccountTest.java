7a3993ce21fa07a8f121d6adabeac2418d8a69e5
Ab-Daniel <31863068+Ab-Daniel@users.noreply.github.com>
2018-11-30 10:26:11 -0700

    Update SavingsAccountTest.java

--- SavingsAccountTest.java
+++ SavingsAccountTest.java
@@ -5,7 +5,26 @@ public class SavingsAccountTest
 	public static void main(String[] args) 
 	{
 		
+		SavingsAccount account = new SavingsAccount(2000, 4.0);
 		
+		System.out.println("Initial Account:");
+		System.out.println(account.toString());
+		
+		account.withdrawButBetter(500);
+		System.out.println("Withdraw: $500");
+		System.out.println(account.toString());
+		
+		account.deposit(4000);
+		System.out.println("Deposit: $4000");
+		System.out.println(account.toString());
+		
+		account.addIntrest();
+		System.out.println("Add Interest:");
+		System.out.println(account.toString());
+		
+		account.withdrawButBetter(3000);
+		System.out.println("Withdraw: $3000");
+		System.out.println(account.toString());
 		
 	}
 	
